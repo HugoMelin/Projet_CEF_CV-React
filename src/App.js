7c608a486714a1a73c73ddcc2e8b6e61c1614821
HugoMelin <hugo.melin01@gmail.com>
2024-05-16 17:50:02 +0200

    Add services content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './header.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faChevronRight, faCircleChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faChevronRight, faCircleChevronUp} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faSquareTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faCopyright } from '@fortawesome/free-regular-svg-icons'
 
 import logo from './img/favicon.png'
 import { Route, Routes, Link } from 'react-router-dom';
 import Home from "./components/Home";
+import Services from "./components/Services"
 
 function Header() {
   return(
@@ -24,10 +25,10 @@ function Header() {
           <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-0">
               <li class="nav-item">
-                <Link to="/" className='nav-link text-light'>Accueil</Link>
+                <Link to="/" className='nav-link'>Accueil</Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Services</a>
+                <Link to="/services" className='nav-link'>Services</Link>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Réalisations</a>
@@ -48,45 +49,46 @@ function Header() {
 
 function Footer() {
   return(
-    <div className='contenair-fluid'>
-      <div className='row m-2'>
-        <div className='col-12 col-md-6 col-xl-3 my-2'>
-          <address>
-            <h3>John Doe</h3>
-            <p>40 Rue Laure Diebold <br/>
-            69009 Lyon, France</p>
-            <p>Téléphone : <a href='tel:0620304050'>06 20 30 40 50</a></p>
-          </address>
-          <div className='fs-1'>
-            <a className='me-3' rel="nofollow" href='https://github.com/github-john-doe' target='blank'>
-              <FontAwesomeIcon icon={faGithub}/>
-            </a>
-            <a className='me-3' rel="nofollow" href='https://twitter.com/home' target='blank'>
-              <FontAwesomeIcon icon={faSquareTwitter}/>
-            </a>
-            <a rel="nofollow" href='https://www.linkedin.com/feed/' target='blank'>
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
+    <div>
+      <div className='container-fluid'>
+        <div className='row m-2'>
+          <div className='col-12 col-md-6 col-xl-3 my-2'>
+            <address>
+              <h4>John Doe</h4>
+              <p>40 Rue Laure Diebold <br/>
+              69009 Lyon, France</p>
+              <p>Téléphone : <a href='tel:0620304050'>06 20 30 40 50</a></p>
+            </address>
+            <div className='fs-1'>
+              <a className='me-3' rel="nofollow" href='https://github.com/github-john-doe' target='blank'>
+                <FontAwesomeIcon icon={faGithub}/>
+              </a>
+              <a className='me-3' rel="nofollow" href='https://twitter.com/home' target='blank'>
+                <FontAwesomeIcon icon={faSquareTwitter}/>
+              </a>
+              <a rel="nofollow" href='https://www.linkedin.com/feed/' target='blank'>
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+            </div>
+          </div>
+          <div className='col-12 col-md-6 col-xl-3'>
+            <h4>Liens utiles</h4>
+            <ul className='footer__list'>
+              <li><Link to="/"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Accueil</Link></li>
+              <li><Link to="/#about"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>A propos</Link></li>
+              <li><Link to="/services"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Services</Link></li>
+              <li><Link to="/contact"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Me contacter</Link></li>
+              <li><Link to="/legal"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Mention légales</Link></li>
+            </ul>
+          </div>
+          <div className='col-12 col-md-6 col-xl-3'>
+            <h4>Mes dernières réalisations</h4>
+          </div>
+          <div className='col-12 col-md-6 col-xl-3'>
+            <h4>Mes derniers articles</h4>
           </div>
         </div>
-        <div className='col-12 col-md-6 col-xl-3'>
-          <h3>Liens utiles</h3>
-          <ul className='footer__list'>
-            <li><Link to="/"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Accueil</Link></li>
-            <li><Link to="/#about"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>A propos</Link></li>
-            <li><Link to="/services"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Services</Link></li>
-            <li><Link to="/contact"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Me contacter</Link></li>
-            <li><Link to="/legal"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Mention légales</Link></li>
-          </ul>
-        </div>
-        <div className='col-12 col-md-6 col-xl-3'>
-          <h3>Mes dernières réalisations</h3>
-        </div>
-        <div className='col-12 col-md-6 col-xl-3'>
-          <h3>Mes derniers articles</h3>
-        </div>
       </div>
-
       <p className='bg-dark text-white text-center py-2'><FontAwesomeIcon icon={faCopyright} /> Designed by John Doe</p>
     </div>
   )
@@ -99,9 +101,10 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/services' element={<Services />}/>
       </Routes>
 
-      <a class="btn btn-secondary position-sticky bottom-0 start-100" href="#top" role="button"><FontAwesomeIcon icon={faCircleChevronUp}/></a>
+      <a class="btn btn-secondary position-sticky bottom-0 start-100 me-1" href="#top" role="button"><FontAwesomeIcon icon={faCircleChevronUp}/></a>
 
       <Footer />
     </div>
