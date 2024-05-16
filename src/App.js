950d9be78c489e112c78a7f1792e06b1d10221bd
HugoMelin <hugo.melin01@gmail.com>
2024-05-16 11:20:37 +0200

    Add footer Content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './header.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faSquareTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import logo from './img/favicon.png'
 import { Route, Routes, Link } from 'react-router-dom';
 import Home from "./components/Home";
@@ -43,6 +44,50 @@ function Header() {
   );
 }
 
+function Footer() {
+  return(
+    <div className='contenair-fluid bg-warning'>
+      <div className='row m-2'>
+        <div className='col-12 col-md-6 col-xl-3 my-2'>
+          <address>
+            <h3>John Doe</h3>
+            <p>40 Rue Laure Diebold <br/>
+            69009 Lyon, France</p>
+            <p>Téléphone : <a href='tel:0620304050'>06 20 30 40 50</a></p>
+          </address>
+          <div className='fs-1'>
+            <a className='me-3' rel="nofollow" href='https://github.com/github-john-doe' target='blank'>
+              <FontAwesomeIcon icon={faGithub}/>
+            </a>
+            <a className='me-3' rel="nofollow" href='https://twitter.com/home' target='blank'>
+              <FontAwesomeIcon icon={faSquareTwitter}/>
+            </a>
+            <a rel="nofollow" href='https://www.linkedin.com/feed/' target='blank'>
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </div>
+        </div>
+        <div className='col-12 col-md-6 col-xl-3'>
+          <h3>Liens utiles</h3>
+          <ul className='footer__list'>
+            <li><Link to="/"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Accueil</Link></li>
+            <li><Link to="/#about"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>A propos</Link></li>
+            <li><Link to="/services"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Services</Link></li>
+            <li><Link to="/contact"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Me contacter</Link></li>
+            <li><Link to="/legal"><FontAwesomeIcon className="pe-2 chevronRight" icon={faChevronRight}/>Mention légales</Link></li>
+          </ul>
+        </div>
+        <div className='col-12 col-md-6 col-xl-3'>
+          <h3>Mes dernières réalisations</h3>
+        </div>
+        <div className='col-12 col-md-6 col-xl-3'>
+          <h3>Mes derniers articles</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -51,6 +96,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
       </Routes>
+
+      <Footer />
     </div>
   );
 }
