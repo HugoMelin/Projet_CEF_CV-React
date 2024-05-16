16b280c582325db00040315bbc7dbf5fce7e6b1f
HugoMelin <hugo.melin01@gmail.com>
2024-05-16 10:22:58 +0200

    Add Home page content

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import './header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import logo from './img/favicon.png'
+import { Route, Routes, Link } from 'react-router-dom';
+import Home from "./components/Home";
 
 function Header() {
   return(
     <header>
       <nav class="navbar navbar-expand-xl bg-dark text-uppercase">
         <div class="container-fluid">
-          <a class="navbar-brand text-light" href="#">
+          <Link to="/" className='navbar-brand text-light'>
             <img src={logo} alt="Logo" width="30" height="24" class="d-inline-block align-text-top me-1"></img>
             John Doe
-          </a>
+          </Link>
           <button class="navbar-toggler border-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <FontAwesomeIcon icon={faBars} className='text-light'/>
           </button>
           <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-0">
               <li class="nav-item">
-                <a class="nav-link active text-light" href="#">Accueil</a>
+                <Link to="/" className='nav-link text-light'>Accueil</Link>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Services</a>
@@ -44,7 +46,11 @@ function Header() {
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header/>
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,72 @@
+import "../home.css"
+import { Link } from "react-router-dom"
+import aboutImg from "../img/john-doe-about.jpg"
+
+function Head() {
+    return(
+        <div className="contenair-fluid text-center bg-image d-flex flex-column justify-content-center" id="header">
+            <h1 className="text-white">Bonjour, je suis John Doe</h1>
+            <h2 className="text-white">Développeur web full stack</h2>
+            <div>
+                <button type="button" class="btn btn-primary"><a href="#about" className="text-white text-decoration-none">En savoir plus</a></button>
+            </div>
+        </div>
+    )
+}
+
+function About() {
+    return(
+        <div id="about" className="container my-3 mx-auto p-3 bg-white shadow">
+            <div className="row">
+                <div className="col-12 col-md-6">
+                    <h3 className="text-capitalize border-bottom border-4 about__title" style={{"--bs-border-color": "#0d6efd"}}>à propos</h3>
+                    <p>Passionné par l'informatique et les nouvelles technologies, j'ai suivi une formation d'<strong>intégrateur-développeur web</strong> au CEF.
+                    Au cours de cette formation, j'ai pu acquérir des bases solides pour travailler dans le domaine du <strong>développement web</strong>.</p>
+                    <p>Basé à Lyon, je suis en recherche d'une alternance au sein d'une agence digitale pour consolider ma formation de <strong>développeur web full stack</strong>.</p>
+                    <p>J'avvorde une attention particulière à la qualité du code que j'écris et je respecte les bonnes pratiques du web.</p>
+                </div>
+                <div className="col-12 col-md-6">
+                    <img src={aboutImg} alt="John Doe working" style={{ width: 100+"%"}} className="rounded"></img>
+                    <h3>Mes compétences</h3>
+                    <div className="container">
+                        <p className="mb-0">HTML5 90%</p>
+                        <div className="row mb-3">
+                            <div className="col-10 rounded-start" style={{height: 1.5+"rem", backgroundColor: "#DB4B4B"}}></div>
+                            <div className="col-2 rounded-end" style={{height: 1.5+"rem", backgroundColor: "#CDCDCD"}}></div>
+                        </div>
+                        <p className="mb-0">CSS3 80%</p>
+                        <div className="row mb-3">
+                            <div className="col-9 rounded-start" style={{height: 1.5+"rem", backgroundColor: "#3CCDDB"}}></div>
+                            <div className="col-3 rounded-end" style={{height: 1.5+"rem", backgroundColor: "#CDCDCD"}}></div>
+                        </div>
+                        <p className="mb-0">JAVASCRIPT 70%</p>
+                        <div className="row mb-3">
+                            <div className="col-8 rounded-start" style={{height: 1.5+"rem", backgroundColor: "#C1C123"}}></div>
+                            <div className="col-4 rounded-end" style={{height: 1.5+"rem", backgroundColor: "#CDCDCD"}}></div>
+                        </div>
+                        <p className="mb-0">PHP 60%</p>
+                        <div className="row mb-3">
+                            <div className="col-7 rounded-start" style={{height: 1.5+"rem", backgroundColor: "#60784E"}}></div>
+                            <div className="col-5 rounded-end" style={{height: 1.5+"rem", backgroundColor: "#CDCDCD"}}></div>
+                        </div>
+                        <p className="mb-0">REACT 50%</p>
+                        <div className="row mb-3">
+                            <div className="col-6 rounded-start" style={{height: 1.5+"rem", backgroundColor: "#2432AA"}}></div>
+                            <div className="col-6 rounded-end" style={{height: 1.5+"rem", backgroundColor: "#CDCDCD"}}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default function Home() {
+    return(
+        <div>
+            <Head/>
+            <About/>
+        </div>
+    )
+}
